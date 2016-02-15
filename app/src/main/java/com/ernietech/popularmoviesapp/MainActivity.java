90f16f2325d324f041db6b05daf49abb8f442912
Ernie <ernie@ernietech.com>
2016-02-15 15:20:39 -0500

    Add dependencies and import MyTheMovieDbApiKey from global gradle.properties file

--- app/src/main/java/com/ernietech/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/ernietech/popularmoviesapp/MainActivity.java
@@ -1,13 +1,19 @@
 package com.ernietech.popularmoviesapp;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String movieAPiKey = BuildConfig.THE_MOVIE_DB_API_KEY;
+        Toast.makeText(getApplicationContext(), "API key: " + movieAPiKey, Toast.LENGTH_SHORT).show();
+
     }
 }
